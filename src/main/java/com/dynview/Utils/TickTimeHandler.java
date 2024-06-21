6c2a3d2c8ca3b91bba27cf4b6404c6c7af93e8db
CommandGenius <rvlcariati@gmail.com>
2024-06-20 22:11:39 -0400

    make it lossy again oops

--- src/main/java/com/dynview/Utils/TickTimeHandler.java
+++ src/main/java/com/dynview/Utils/TickTimeHandler.java
@@ -34,7 +34,7 @@ public class TickTimeHandler
 
         if (tickTimer % 20 == 0)
         {
-            meanTickSum += (int) (average(server.getTickTimesNanos()) * 1.0E-6D);
+            meanTickSum += average(server.getTickTimesNanos()) * 1.0E-6D;
             meanTickCount++;
 
             if (tickTimer >= serverTickTimerInterval)
