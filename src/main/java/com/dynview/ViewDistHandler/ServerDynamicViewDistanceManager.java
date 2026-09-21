7dc00c7fc33ff5cac759face8b0c5e575a27bd97
someaddons <someaddons@github.com>
2026-09-21 22:43:05 +0200

    4.1 Fix update interval in the config not respected Fix simulation distance config disable not working Fix singleplayer chunk ignored warning logging

--- src/main/java/com/dynview/ViewDistHandler/ServerDynamicViewDistanceManager.java
+++ src/main/java/com/dynview/ViewDistHandler/ServerDynamicViewDistanceManager.java
@@ -58,7 +58,7 @@ public class ServerDynamicViewDistanceManager implements IDynamicViewDistanceMan
 
         if (meanTickTime - UPDATE_LEEWAY > DynView.config.getCommonConfig().meanAvgTickTime)
         {
-            if (currentChunkUpdateDist > DynView.config.getCommonConfig().minSimulationDist && rand.nextInt(10) != 0)
+            if (DynView.config.getCommonConfig().adjustSimulationDistance && currentChunkUpdateDist > DynView.config.getCommonConfig().minSimulationDist && rand.nextInt(10) != 0)
             {
                 currentChunkUpdateDist--;
                 if (DynView.config.getCommonConfig().logMessages)
